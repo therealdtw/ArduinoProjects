d81476f1ca0dbc10c9e694e94614894d743f0a2f
therealdtw <david@aberrant.ca>
2020-10-09 10:56:27 -0700

    Update StrangerThingsLights.ino
    Just cleaned it up a bit

--- Halloween/StrangerThingsLights.ino
+++ Halloween/StrangerThingsLights.ino
@@ -1,34 +1,9 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+ Created from the base of http://www.arduino.cc/en/Tutorial/Blink
+ 
 */
-
-//  static int RELAY_1 = 2;
-//  static int RELAY_2 = 3;
-//  static int RELAY_3 = 4;
-//  static int RELAY_4 = 5;
-//  static int RELAY_5 = 6;
-//  static int RELAY_6 = 7;
-
+  
+  // PIN to relay mapping
   static int RELAY_1 = 5;
   static int RELAY_2 = 6;
   static int RELAY_3 = 7;
